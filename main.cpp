1997b50147866b4bfd56c9810648bafc1830d98b
Elliott Beach <elliott2.71828@gmail.com>
2017-10-22 22:36:08 -0500

    reformat code

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ typedef void *(*thread_func)(void *);
 struct TCB {
     sigjmp_buf env;
     thread_func function;
-    void* arg;
-    void* result;
+    void *arg;
+    void *result;
     int waiting_for_tid = -1;
     bool complete;
 };
@@ -66,11 +66,11 @@ void test_uthread_suspend();
 
 void test_timing();
 
-void finish(){
+void finish() {
     siglongjmp(main_env, 1);
 }
 
-bool valid_tid(int tid){
+bool valid_tid(int tid) {
     return 0 <= tid && tid < num_threads;
 }
 
@@ -80,10 +80,10 @@ bool valid_tid(int tid){
  * @param id - The id to remove.
  * @return true if the item was removec, else false.
  */
-bool remove(std::deque<int> items, int num){
+bool remove(std::deque<int> items, int num) {
     std::deque<int>::iterator it;
-    if((it = find(items.begin(), items.end(), num)) != items.end()) {
-	items.erase(it);
+    if ((it = find(items.begin(), items.end(), num)) != items.end()) {
+        items.erase(it);
         return true;
     }
     return false;
@@ -94,39 +94,38 @@ bool remove(std::deque<int> items, int num){
     Use this as a black box in your code. */
 #ifdef __x86_64__
 
-    /* 64 bit Intel arch */
+/* 64 bit Intel arch */
 
-    typedef unsigned long address_t;
-    #define JB_SP 6
-    #define JB_PC 7
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
 
-    address_t translate_address(address_t addr)
-    {
-        address_t ret;
-        asm volatile("xor    %%fs:0x30,%0\n"
-                "rol    $0x11,%0\n"
-                : "=g" (ret)
-                : "0" (addr));
-        return ret;
-    }
+address_t translate_address(address_t addr) {
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+            "rol    $0x11,%0\n"
+    : "=g" (ret)
+    : "0" (addr));
+    return ret;
+}
 
 #else
 
-    /* 32 bit Intel arch */
+/* 32 bit Intel arch */
 
-    typedef unsigned int address_t;
-    #define JB_BP 3
-    #define JB_SP 4
-    #define JB_PC 5
+typedef unsigned int address_t;
+#define JB_BP 3
+#define JB_SP 4
+#define JB_PC 5
 
-    address_t translate_address(address_t addr){
-        address_t ret;
-        asm volatile("xor    %%gs:0x18,%0\n"
-                "rol    $0x9,%0\n"
-                : "=g" (ret)
-                : "0" (addr));
-        return ret;
-    }
+address_t translate_address(address_t addr){
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+            "rol    $0x9,%0\n"
+            : "=g" (ret)
+            : "0" (addr));
+    return ret;
+}
 
 #endif
 
@@ -137,12 +136,12 @@ void disable_interrupts() {
 
     // Block interrupts
     sigset_t blockmask;
-    if((sigemptyset(&blockmask) == -1) || (sigaddset(&blockmask, SIGVTALRM) == -1)) {
-	perror("Failed to initialize signal set");
-	exit(0);
-    } else if(sigprocmask(SIG_BLOCK, &blockmask, nullptr) == -1) {
-	perror("Failed to block interrupt");
-	exit(0);
+    if ((sigemptyset(&blockmask) == -1) || (sigaddset(&blockmask, SIGVTALRM) == -1)) {
+        perror("Failed to initialize signal set");
+        exit(0);
+    } else if (sigprocmask(SIG_BLOCK, &blockmask, nullptr) == -1) {
+        perror("Failed to block interrupt");
+        exit(0);
     }
 }
 
@@ -151,15 +150,15 @@ void disable_interrupts() {
  */
 void enable_interrupts() {
     sigset_t blockmask;
-    if((sigemptyset(&blockmask) == -1) || (sigaddset(&blockmask, SIGVTALRM) == -1)) {
-	perror("Failed to intialize signal set");
-	exit(0);
+    if ((sigemptyset(&blockmask) == -1) || (sigaddset(&blockmask, SIGVTALRM) == -1)) {
+        perror("Failed to intialize signal set");
+        exit(0);
     }
 
     // Unblock interrupts
-    if(sigprocmask(SIG_UNBLOCK, &blockmask, nullptr) == -1) {
-	perror("Failed to unblock interrupt");
-	exit(0);
+    if (sigprocmask(SIG_UNBLOCK, &blockmask, nullptr) == -1) {
+        perror("Failed to unblock interrupt");
+        exit(0);
     }
 }
 
@@ -173,14 +172,14 @@ void free_waiting_threads(int tid) {
     // Search for threads waiting on this thread - set them to ready
     std::deque<int>::iterator it = waiting_list.begin();
     std::deque<int>::iterator end = waiting_list.end();
-    while(it != end) {
-	int id = *it;
-	if(threads[id].waiting_for_tid == tid) {
-	    threads[id].waiting_for_tid = -1;
-	    waiting_list.erase(it);
-	    ready_list.push_back(id);
-	}
-	++it;
+    while (it != end) {
+        int id = *it;
+        if (threads[id].waiting_for_tid == tid) {
+            threads[id].waiting_for_tid = -1;
+            waiting_list.erase(it);
+            ready_list.push_back(id);
+        }
+        ++it;
     }
 
 }
@@ -188,26 +187,26 @@ void free_waiting_threads(int tid) {
 /**
  * Completes a thread's execution. adds to the ready list.
  */
-void thread_complete(){
+void thread_complete() {
 
     disable_interrupts();
 
-    int ret_val = sigsetjmp(threads[current_thread_id].env,1);
+    int ret_val = sigsetjmp(threads[current_thread_id].env, 1);
     if (ret_val == 1) {
-	enable_interrupts();
-	return;
+        enable_interrupts();
+        return;
     }
 
     // After this is set, thread will never execute again.
-    TCB* tcb = &threads[current_thread_id];
+    TCB *tcb = &threads[current_thread_id];
     tcb->complete = true;
 
     free_waiting_threads(current_thread_id);
 
     // If all threads have complete execution
-    if(ready_list.empty()) {
+    if (ready_list.empty()) {
         enable_interrupts();
-	    finish();
+        finish();
     }
 
     // Choose the first thread on the ready list
@@ -216,7 +215,7 @@ void thread_complete(){
 
     enable_interrupts();
 
-    siglongjmp(threads[current_thread_id].env,1);
+    siglongjmp(threads[current_thread_id].env, 1);
 }
 
 /**
@@ -224,8 +223,8 @@ void thread_complete(){
  * Calls the thread function with its argument, marks the thread as complete, and yields.
  * @param arg not used
  */
-void thread_wrapper(void *arg){
-    TCB* tcb = &threads[current_thread_id];
+void thread_wrapper(void *arg) {
+    TCB *tcb = &threads[current_thread_id];
     tcb->result = tcb->function(tcb->arg);
 
     thread_complete();
@@ -237,30 +236,30 @@ void thread_wrapper(void *arg){
  * @param arg - An argument to pass to the function.
  * @return The tid of the created thread, or -1 if there were too many threads.
  */
-int uthread_create(void *(start_routine)(void *), void* arg){
+int uthread_create(void *(start_routine)(void *), void *arg) {
 
     disable_interrupts();
-    if(num_threads >= 999){
+    if (num_threads >= 999) {
         enable_interrupts();
         return -1;
     }
 
     int tid = num_threads;
-    TCB* tcb = &threads[tid];
+    TCB *tcb = &threads[tid];
 
     // Store the arg and function in a global variable.
     tcb->function = start_routine;
     tcb->arg = arg;
 
     // Allocate the stack.
-    auto* stack = (char*)malloc(STACK_SIZE);
+    auto *stack = (char *) malloc(STACK_SIZE);
 
     // sp starts out at the top of the stack, pc at the wrapper function.
-    auto sp = (address_t)stack + STACK_SIZE - 10 * sizeof(void*);
-    auto pc = (address_t)thread_wrapper;
+    auto sp = (address_t) stack + STACK_SIZE - 10 * sizeof(void *);
+    auto pc = (address_t) thread_wrapper;
 
     // Modify the env_buf with the thread context.
-    sigsetjmp(tcb->env,1);
+    sigsetjmp(tcb->env, 1);
     (tcb->env->__jmpbuf)[JB_SP] = translate_address(sp);
     (tcb->env->__jmpbuf)[JB_PC] = translate_address(pc);
     sigemptyset(&tcb->env->__saved_mask);
@@ -276,15 +275,15 @@ int uthread_create(void *(start_routine)(void *), void* arg){
     return tid;
 }
 
-void uthread_yield(){
+void uthread_yield() {
 
     disable_interrupts();
 
     // Save execution state.
-    int ret_val = sigsetjmp(threads[current_thread_id].env,1);
+    int ret_val = sigsetjmp(threads[current_thread_id].env, 1);
     if (ret_val == 1) {
-	enable_interrupts();
-	return;
+        enable_interrupts();
+        return;
     }
 
     // Choose the next thread to execute.
@@ -295,30 +294,30 @@ void uthread_yield(){
 
     enable_interrupts();
 
-    siglongjmp(threads[current_thread_id].env,1);
+    siglongjmp(threads[current_thread_id].env, 1);
 }
 
 /**
  * Add calling thread to the waiting list to block. Switch to a
  * different thread.
  */
-void thread_switch(){
+void thread_switch() {
 
     disable_interrupts();
 
-    int ret_val = sigsetjmp(threads[current_thread_id].env,1);
+    int ret_val = sigsetjmp(threads[current_thread_id].env, 1);
     if (ret_val == 1) {
-	enable_interrupts();
-	return;
+        enable_interrupts();
+        return;
     }
 
     // Place calling thread on waiting list
     waiting_list.push_back(current_thread_id);
 
     // Deadlock
-    if(ready_list.empty()) {
+    if (ready_list.empty()) {
         enable_interrupts();
-	   finish();
+        finish();
     }
 
     // Take the top thread off the ready list
@@ -327,10 +326,10 @@ void thread_switch(){
 
     enable_interrupts();
 
-    siglongjmp(threads[current_thread_id].env,1);
+    siglongjmp(threads[current_thread_id].env, 1);
 }
 
-int uthread_self(){
+int uthread_self() {
     return current_thread_id;
 }
 
@@ -340,9 +339,9 @@ int uthread_self(){
  * @param retval A pointer which will be set to the return value of the thread.
  * @return 0 if join was successful, false if tid did not represent a valid thread.
  */
-int uthread_join(int tid, void **retval){
-    if(!valid_tid(tid)) return -1;
-    if(tid == current_thread_id || threads[tid].complete) return 0;
+int uthread_join(int tid, void **retval) {
+    if (!valid_tid(tid)) return -1;
+    if (tid == current_thread_id || threads[tid].complete) return 0;
     threads[current_thread_id].waiting_for_tid = tid;
     thread_switch();
     *retval = threads[tid].result;
@@ -355,14 +354,14 @@ int uthread_join(int tid, void **retval){
  * @param buf The buffer to read into.
  * @param nbytes Number of bytes to read.
  */
-ssize_t async_read(int fildes, void *buf, size_t nbytes){
+ssize_t async_read(int fildes, void *buf, size_t nbytes) {
     struct aiocb params{};
     params.aio_fildes = fildes;
     params.aio_buf = buf;
     params.aio_nbytes = nbytes;
-    if(aio_read(&params) != 0) return -1;
-    while(true){
-        switch(aio_error(&params)){
+    if (aio_read(&params) != 0) return -1;
+    while (true) {
+        switch (aio_error(&params)) {
             case EINPROGRESS:
                 uthread_yield();
                 continue;
@@ -381,20 +380,20 @@ ssize_t async_read(int fildes, void *buf, size_t nbytes){
  */
 int uthread_resume(int tid) {
 
-    if(!valid_tid(tid)) return -1;
+    if (!valid_tid(tid)) return -1;
 
     enable_interrupts();
 
     std::deque<int>::iterator it;
     // Verify that tid is currently suspended
-    if((it = find(suspended_list.begin(), suspended_list.end(), tid)) != suspended_list.end()) {
-	suspended_list.erase(it);
-	if(threads[tid].waiting_for_tid == -1)
-	    ready_list.push_back(tid);
-	else
-	    waiting_list.push_back(tid);
-	enable_interrupts();
-	return 0;
+    if ((it = find(suspended_list.begin(), suspended_list.end(), tid)) != suspended_list.end()) {
+        suspended_list.erase(it);
+        if (threads[tid].waiting_for_tid == -1)
+            ready_list.push_back(tid);
+        else
+            waiting_list.push_back(tid);
+        enable_interrupts();
+        return 0;
     }
 
     enable_interrupts();
@@ -409,40 +408,39 @@ int uthread_resume(int tid) {
  */
 int uthread_suspend(int tid) {
     // Verify that tid is valid
-    if(!valid_tid(tid)) return -1;
+    if (!valid_tid(tid)) return -1;
     disable_interrupts();
     // If tid is complete it can't be suspended
-    if(threads[tid].complete) {
+    if (threads[tid].complete) {
         enable_interrupts();
         return -1;
     }
 
     // If a thread tries to suspend itself
-    if(tid == current_thread_id) {
-	suspended_list.push_back(current_thread_id);
-	thread_switch();
-	return 0;
+    if (tid == current_thread_id) {
+        suspended_list.push_back(current_thread_id);
+        thread_switch();
+        return 0;
     }
 
     std::deque<int>::iterator it;
 
     // Check if the tid trying to be suspended is in the ready list or the waiting list
-    if((it = find(ready_list.begin(), ready_list.end(), tid)) != ready_list.end()) {
-	ready_list.erase(it);
-	suspended_list.push_back(tid);
-    } else if((it = find(waiting_list.begin(), waiting_list.end(), tid)) != waiting_list.end()) {
-	waiting_list.erase(it);
-	suspended_list.push_back(tid);
+    if ((it = find(ready_list.begin(), ready_list.end(), tid)) != ready_list.end()) {
+        ready_list.erase(it);
+        suspended_list.push_back(tid);
+    } else if ((it = find(waiting_list.begin(), waiting_list.end(), tid)) != waiting_list.end()) {
+        waiting_list.erase(it);
+        suspended_list.push_back(tid);
     } else {
-	enable_interrupts();
-	return -1;  // Handles case if tid is already suspended
+        enable_interrupts();
+        return -1;  // Handles case if tid is already suspended
     }
 
     enable_interrupts();
 
     return 0;
 }
-
 
 
 /**
@@ -452,11 +450,11 @@ int uthread_suspend(int tid) {
  */
 int uthread_terminate(int tid) {
 
-    if(!valid_tid(tid)) return -1;
+    if (!valid_tid(tid)) return -1;
 
     disable_interrupts();
 
-    if(tid == current_thread_id) {
+    if (tid == current_thread_id) {
         // We could enable interrupts here, but it is safer to not,
         // and thread_complete will disable interrupts immediatly anyway.
         thread_complete();
@@ -512,21 +510,21 @@ int setupinterrupt(void) {
 /**
  * Start the threading library.
  */
-void start(){
-    if(sigsetjmp(main_env,1) != 0){
+void start() {
+    if (sigsetjmp(main_env, 1) != 0) {
         return;
     }
 
-    if(setupinterrupt() == -1) {
-	perror("Failed to set up handler");
+    if (setupinterrupt() == -1) {
+        perror("Failed to set up handler");
     }
-    if(setupitimer() == -1) {
-	perror("Failed to set up timer");
+    if (setupitimer() == -1) {
+        perror("Failed to set up timer");
     }
 
     current_thread_id = ready_list.front();
     ready_list.pop_front();
-    siglongjmp(threads[current_thread_id].env,1);
+    siglongjmp(threads[current_thread_id].env, 1);
 }
 
 ////////////////////////////////////
@@ -535,15 +533,15 @@ void start(){
 
 // Fixtures
 
-void* uthread_argument_fixture(void *arg){
-    assert((long)arg == 10);
+void *uthread_argument_fixture(void *arg) {
+    assert((long) arg == 10);
     return 0;
 }
 
-void* uthread_yield_fixture(void *arg){
+void *uthread_yield_fixture(void *arg) {
     int limit = 5;
-    for(int i=0;i<2*limit;i++){
-        if(i % limit == 0){
+    for (int i = 0; i < 2 * limit; i++) {
+        if (i % limit == 0) {
             uthread_yield();
         }
         printf("%d\n", i);
@@ -551,46 +549,45 @@ void* uthread_yield_fixture(void *arg){
     return 0;
 }
 
-void* yield_fixture(void *arg){
+void *yield_fixture(void *arg) {
     uthread_yield();
     return 0;
 }
 
-void* uthread_self_fixture(void *arg){
+void *uthread_self_fixture(void *arg) {
     static int id = 0;
-    assert(uthread_self()==id);
+    assert(uthread_self() == id);
     id++;
     return nullptr;
 }
 
-void* return_10_fixture(void* arg){
-   return (void*)10l;
+void *return_10_fixture(void *arg) {
+    return (void *) 10l;
 }
 
-void* test_timing_fixture(void* arg){
+void *test_timing_fixture(void *arg) {
     int limit = 5;
     static int i = 0;
     i++;
-    if (i < limit){
+    if (i < limit) {
         uthread_create(test_timing_fixture, nullptr);
         // If we can get through this while loop, timing is working.
-        while(i < limit)
-            ;
+        while (i < limit);
     }
     return 0;
 }
 
-void* uthread_join_fixture(void *arg){
-    void* retval;
+void *uthread_join_fixture(void *arg) {
+    void *retval;
     int tid = uthread_create(return_10_fixture, nullptr);
     uthread_join(tid, &retval);
-    assert((long)retval == 10l);
+    assert((long) retval == 10l);
     return 0;
 }
 
-void* nop_fixture(void *arg){}
+void *nop_fixture(void *arg) {}
 
-void* uthread_suspend_fixture(void *arg) {
+void *uthread_suspend_fixture(void *arg) {
     int tid = uthread_create(nop_fixture, nullptr);
     uthread_suspend(tid);
     uthread_yield();
@@ -600,8 +597,8 @@ void* uthread_suspend_fixture(void *arg) {
     assert(threads[tid].complete);
 }
 
-void* async_read_fixture(void *arg){
-    char buf [1024];
+void *async_read_fixture(void *arg) {
+    char buf[1024];
     int tid1 = uthread_create(yield_fixture, nullptr);
     int tid2 = uthread_create(yield_fixture, nullptr);
     ssize_t result = async_read(open("/etc/passwd", O_RDONLY), &buf, 100);
@@ -611,13 +608,13 @@ void* async_read_fixture(void *arg){
 }
 
 // Test that we cannot join on an invalid tid.
-void* uthread_join_invalid_tid_fixture(void *arg){
-    void* retval;
+void *uthread_join_invalid_tid_fixture(void *arg) {
+    void *retval;
     int ret = uthread_join(-1, &retval);
     assert(ret != 0);
 }
 
-void* test_uthread_yield_fixture(void* arg){
+void *test_uthread_yield_fixture(void *arg) {
     int tid = uthread_create(uthread_self_fixture, nullptr);
     assert(!threads[tid].complete);
     uthread_yield();
@@ -628,35 +625,35 @@ void* test_uthread_yield_fixture(void* arg){
 
 // Test that creating a uthread can start a thread, passing
 // an argument.
-void test_uthread_create(){
+void test_uthread_create() {
     uthread_create(uthread_argument_fixture, (void *) 10l);
     start();
 }
 
 // Test that creating a thread and calling
 // `uthread_start` gives the expected result.
-void test_thread_self(){
+void test_thread_self() {
     uthread_create(uthread_self_fixture, nullptr);
     start();
 }
 
 // Test that creating a thread and joining captures
 // the return value of the thread.
-void test_uthread_join(){
+void test_uthread_join() {
     uthread_create(uthread_join_fixture, nullptr);
     start();
 }
 
 // Test that yielding causes another thread to run before the next
 // statement.
-void test_uthread_yield(){
+void test_uthread_yield() {
     uthread_create(test_uthread_yield_fixture, nullptr);
     start();
 }
 
 // Test that joining a thread on an invalid tid does not cause
 // the joining thread to block forever.
-void* test_join_invalid_tid(){
+void *test_join_invalid_tid() {
     int id = uthread_create(uthread_join_invalid_tid_fixture, nullptr);
     start();
     assert(threads[id].complete);
@@ -671,7 +668,7 @@ void test_uthread_suspend() {
 
 // Test that reading asynchronously succeeds but completes
 // after letting other threads finish.
-void* test_async(){
+void *test_async() {
     int tid = uthread_create(async_read_fixture, nullptr);
     start();
     assert(reinterpret_cast<long>(threads[tid].result) == 100);
@@ -683,7 +680,7 @@ void test_timing() {
     start();
 }
 
-int main(){
+int main() {
     test_thread_self();
     test_uthread_create();
     test_uthread_join();
